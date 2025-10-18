7ed77e8704163407e3a310eb97d628316c8ee8cd
Coder Irfan <mohdirfan70097@gmail.com>
2025-10-18 21:06:23 +0530

    Updating Host url 5

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "0.0.0.0", // ✅ allows external access
+    host: "::",
     port: 8080,
-    allowedHosts: [
-      "javacoder-ai-personalize-platform-tech.onrender.com", // ✅ allow Render host
-    ],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
